9a64909ea5a5fcbf5765a85453f7cd79d376056b
johnson-oragui <johnson.oragui@gmail.com>
2024-10-29 23:22:04 +0100

    added comment model

---
+++ api/models/comment.js
@@ -0,0 +1,64 @@
+const { DataTypes, Model } = require("sequelize");
+const { v4: uuidv4, stringify } = require("uuid");
+const sequelize = require("../database/database");
+
+class Comment extends Model { }
+
+Comment.init(
+    {
+        id: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+            primaryKey: true,
+            defaultValue: () => uuidv4().toString(),
+        },
+        content: {
+            type: DataTypes.TEXT,
+            allowNull: false,
+        },
+        blog_id: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            references: {
+                model: "blogs",
+                key: "id",
+            },
+        },
+        user_id: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            references: {
+                model: "users",
+                key: "id",
+            },
+        },
+    },
+    {
+        sequelize,
+        modelName: "Comment",
+        tableName: "comments",
+        indexes: [
+            {
+                fields: ["blog_id"], // Index on blog_id for efficient access
+            },
+            {
+                fields: ["user_id"],
+            }
+        ],
+    }
+);
+
+(async () => {
+    try {
+        await Comment.sync({ alter: true });
+        console.log("The table for the Comment model was just (re)created!");
+    } catch (error) {
+        console.error("Unable to synchronize models:", error);
+    }
+})();
+
+// `sequelize.define` also returns the model
+console.log(Comment === sequelize.models.Comment); // true
+
+module.exports = Comment;
