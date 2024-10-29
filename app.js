5d53ab64bd2f547a796a76dada90698a5f2c803a
johnson-oragui <johnson.oragui@gmail.com>
2024-10-29 20:30:12 +0100

    added the main entry of the API server

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const dotenv = require("dotenv");
+
+// Loads the env variables
+dotenv.config();
+
+// Create an instance of the express server
+const app = express();
+
+// enable the parsing of JSON in the request body
+app.use(express.json());
+
+
+// setup a port for the app from env file or 5000
+const PORT = process.env.PORT || 5000;
+
+// listen for requests
+app.listen(PORT, () => {
+    console.log(`App listening op http://127.0.0.1:${PORT}`);
+});
