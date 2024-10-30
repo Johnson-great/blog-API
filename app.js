ebadebc1e19b5d01b3e0fce538e06a127618892a
johnson-oragui <johnson.oragui@gmail.com>
2024-10-30 13:03:29 +0100

    asetupcors middleware

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require("express");
+const cors = require('cors')
 const dotenv = require("dotenv");
 
 require("./api/models/modelsSync");
+const router = require("./api/routes/router");
 
 // Loads the env variables
 dotenv.config();
@@ -12,6 +14,16 @@ const app = express();
 // enable the parsing of JSON in the request body
 app.use(express.json());
 
+app.use(cors(
+    {
+        origin: "*",
+        methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Allowed HTTP methods
+        credentials: true, // Allow credentials (cookies, authorization headers, etc.)
+        optionsSuccessStatus: 200, // Respond with 200 for preflight requests
+    }
+));
+
+app.use("/", router);
 
 // setup a port for the app from env file or 5000
 const PORT = process.env.PORT || 5000;
